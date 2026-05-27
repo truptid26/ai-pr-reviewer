f60eccbf84f13ecea4c7bddf1cec2a1e72a12626
Trupti Deokar <truptideokar@freightify.com>
2026-05-27 09:44:39 +0530

    ci:added test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 70,
+        functions: 70,
+        branches: 70,
+        statements: 70,
+      },
+    },
+  },
+});
